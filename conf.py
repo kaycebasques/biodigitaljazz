d788aefac3d0f6feda1b132a1ae8a958cb4f3fd2
Kayce Basques <kayce@google.com>
2023-11-22 12:09:47 -0800

    Make Sphinx ignore venv dir

--- conf.py
+++ conf.py
@@ -5,7 +5,7 @@ release = '0.0.0'
 
 extensions = []
 templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'venv']
 
 html_theme = 'alabaster'
 html_static_path = ['_static']

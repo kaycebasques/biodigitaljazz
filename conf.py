12f6c66e9dc54560e4462afe26c693be719bc50a
Kayce Basques <kayce@google.com>
2023-11-22 12:07:53 -0800

    Init Sphinx stuff

---
+++ conf.py
@@ -0,0 +1,11 @@
+project = 'biodigitaljazz'
+copyright = '2023, Kayce Basques'
+author = 'Kayce Basques'
+release = '0.0.0'
+
+extensions = []
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+html_theme = 'alabaster'
+html_static_path = ['_static']

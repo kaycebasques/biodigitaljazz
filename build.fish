2b6abe04c5016e19a8e05dabc615745512914f56
Kayce Basques <kayce@google.com>
2025-03-21 16:13:11 -0700

    Update pip when building

--- build.fish
+++ build.fish
@@ -1,5 +1,6 @@
 if not test -d venv; python3 -m venv venv; end
 . venv/bin/activate.fish
+python3 -m pip install --upgrade pip
 python3 -m pip install -r requirements.txt
 if test -d _build; rm -rf _build; end
 set SRC_DIR .
